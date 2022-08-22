20a24c03523b4225957bcf629282852d38e840db
Mathieu <mathieu.collet@gmail.com>
2022-08-22 15:14:00 +0200

    Base style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      red: "hsl(0, 100%, 66%)",
+      white: "hsl(0, 0%, 100%)",
+      'violet': {
+        "light-grayish": "hsl(270, 3%, 87%)",
+        "dark-grayish": "hsl(279, 6%, 55%)",
+        blueish: "hsl(249, 99%, 64%)",
+        DEFAULT: "hsl(278, 94%, 30%)",
+        dark: "hsl(278, 68%, 11%)",
+      }
+    },
+    extend: {
+      fontFamily: {
+        Space: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
